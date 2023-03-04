03d67328bbcd9d26d24299b6a945a5bdd81bfeb3
ettoreciarcia <ettoreciarcia.tech@gmail.com>
2023-03-04 10:14:37 +0100

    Added bird watcher exercise

---
+++ bird-watcher/bird_watcher.go
@@ -0,0 +1,34 @@
+package birdwatcher
+
+// TotalBirdCount return the total bird count by summing
+// the individual day's counts.
+func TotalBirdCount(birdsPerDay []int) int {
+	sum := 0
+	for i := 0; i < len(birdsPerDay); i++ {
+		sum += birdsPerDay[i]
+	}
+	return sum
+}
+
+// BirdsInWeek returns the total bird count by summing
+// only the items belonging to the given week.
+func BirdsInWeek(birdsPerDay []int, week int) int {
+	var offset int
+	var sum int
+	if week == 2 {
+		offset = 7
+	}
+	for i := 0; i < 7; i++ {
+		sum += birdsPerDay[offset+i]
+	}
+	return sum
+}
+
+// FixBirdCountLog returns the bird counts after correcting
+// the bird counts for alternate days.
+func FixBirdCountLog(birdsPerDay []int) []int {
+	for i := 0; i < len(birdsPerDay); i += 2 {
+		birdsPerDay[i] = birdsPerDay[i] + 1
+	}
+	return birdsPerDay
+}
